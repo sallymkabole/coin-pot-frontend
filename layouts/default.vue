1ad25f1158395ab51e2a587754bea210be086e60
sallythetechie <sllmusanga7@gmail.com>
2021-02-23 04:53:29 +0300

    Edit side nav and add google font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,153 +1,131 @@
 <template>
-  <v-app class="bg ">
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-      class="nav-drawer"
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container >
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; CoinPot {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+      <v-app class="bg">
+        <v-app-bar app color="primary" dark elevation="0">
+            <v-app-bar-nav-icon @click.stop="sidebarMenu = !sidebarMenu"></v-app-bar-nav-icon>
+            <v-spacer></v-spacer>
+            <v-icon>mdi-account</v-icon>
+        </v-app-bar>
+        <v-navigation-drawer 
+            v-model="sidebarMenu" 
+            app
+            floating
+            :permanent="sidebarMenu"
+            :mini-variant.sync="mini"
+            class="nav-drawer"
+            >
+            <v-list dense color="primary" dark>
+                <v-list-item>
+                    <v-list-item-action>
+                        <v-icon @click.stop="sidebarMenu = !sidebarMenu">mdi-chevron-left</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            <h3 class="font-weight-thin">Vuetify Dashboard</h3>
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <v-list-item class="px-2" @click="toggleMini = !toggleMini">
+                <v-list-item-avatar>
+                    <v-icon  class="white">mdi-account-outline</v-icon>
+                </v-list-item-avatar>
+                <v-list-item-content class="text-truncate white--text">
+                    Jane Githinji
+                </v-list-item-content>
+                <v-btn icon small>
+                    <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list>
+                <v-list-item v-for="item in items" :key="item.title" link :to="item.href">
+                    <v-list-item-icon>
+                        <v-icon color="white">{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text font-weight-thin">{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-content>
+            <v-container class="px-4 py-0 fill-height" fluid>
+                <nuxt/>
+            </v-container>
+        </v-content>
+        <v-footer class="py-3">
+            <span class="ml-auto overline">CoinPot &copy;2021</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+    computed: {
+    mini() {
+        return (this.$vuetify.breakpoint.smAndDown) || this.toggleMini
+    },
+    buttonText() {
+        return !this.$vuetify.theme.dark ? 'Go Dark' : 'Go Light'
+    }
+  },
+  data() {
     return {
-      clipped: false,
-      drawer: false,
+      sidebarMenu: true,
+      toggleMini: false,
       fixed: false,
+      sidebarMenu: true,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Buy Coins',
-          to: '/buycoins'
+          icon: "mdi-chart-bubble",
+          title: "Buy Coins",
+          to: "/buycoins",
         },
-         {
-          icon: 'mdi-chart-bubble',
-          title: 'Sell Coins',
-          to: '/sellcoins'
+        {
+          icon: "mdi-chart-bubble",
+          title: "Sell Coins",
+          to: "/sellcoins",
         },
-         {
-          icon: 'mdi-chart-bubble',
-          title: 'Bought Coins',
-          to: '/boughtcoins'
+        {
+          icon: "mdi-chart-bubble",
+          title: "Bought Coins",
+          to: "/boughtcoins",
         },
-         {
-          icon: 'mdi-chart-bubble',
-          title: ' Referrals',
-          to: '/referrals'
+        {
+          icon: "mdi-chart-bubble",
+          title: " Referrals",
+          to: "/referrals",
         },
-         {
-          icon: 'mdi-chart-bubble',
-          title: 'How It Works',
-          to: '/howitworks'
+        {
+          icon: "mdi-chart-bubble",
+          title: "How It Works",
+          to: "/howitworks",
         },
-         {
-          icon: 'mdi-chart-bubble',
-          title: 'My Wallet',
-          to: '/mywallet'
-        }
+        {
+          icon: "mdi-chart-bubble",
+          title: "My Wallet",
+          to: "/mywallet",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      title: "Vuetify.js",
+    };
+  },
+};
 </script>
 <style scoped>
-.bg{
-  background-color: #161D31;
+.bg {
+  background-color: #161d31;
 }
-.nav-drawer{
-  background-color: #F7F7FF;
-  
+.nav-drawer {
+  background-color: #26314c;
 }
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,224 +1,201 @@
 <template>
-  <v-container>
+  <v-container class="txt">
     <v-flex>
-      <v-card height="300" class="mx-auto text-center" color="#26314C" dark>
-        <v-card-text class="">
-          <div class="display-1 font-weight-bold">My Stats</div>
-        </v-card-text>
+      <v-card class="mx-auto text-center" color="#26314C" dark>
+        <v-card-title class="white--text display-1  text-uppercase">
+          My Stats
+        </v-card-title>
         <v-sparkline
-          :value="value1"
-          color="rgba(255, 255, 255, .7)"
-          :height="48"
-          line-width="0.5"
+          :value="sparklineData"
+          padding="18"
+          label-size="4"
+          color="cyan"
+          :gradient="['#007bff', 'cyan']"
+          :line-width="1"
+          :stroke-linecap="'round'"
         >
         </v-sparkline>
       </v-card>
     </v-flex>
 
-    <v-layout row  class="mt-2">
-      <v-flex  class="pa-2 " md4 sm12>
-        <v-card color="#26314C" height="200">
-          <v-card-text class="">
-            <v-card-actions class="center--text" >
-            <v-list-item class="grow ">
-              <v-list-item-avatar color="#ff8133">
-                <v-icon class="mr-1 white--text"> mdi-cash-plus </v-icon>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-bold white--text">
-                  Earnings
-                </v-list-item-title>
-                <v-list-item-subtitle class="white--text"
-                  >Ksh 256</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-card-actions>
-          </v-card-text>
+    <v-layout row class="mt-2">
+      <v-flex class="pa-2" md4 sm12>
+        <v-card color="#26314C" height="100">
+          <v-layout row justify-space-between class="ma-0">
+            <v-flex md12 class="vl" >
+              <div class="pa-6">
+                <div class="headline font-weight-thin white--text">Earnings</div>
+                <div class="font-weight-thin white--text"> Ksh 1234</div>
+              </div>
+            </v-flex>
+            
+          </v-layout>
         </v-card>
       </v-flex>
       <v-flex class="pa-2" md4 sm12>
-        <v-card color="#26314C" height="200">
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="#ff8133">
-                <v-icon class="mr-1 white--text"> mdi-cash-minus </v-icon>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-bold white--text">
-                  Expenses
-                </v-list-item-title>
-                <v-list-item-subtitle class="white--text"
-                  >Ksh 256</v-list-item-subtitle
-                >
-              </v-list-item-content>
-
-              <v-row align="center" justify="end"> </v-row>
-            </v-list-item>
-          </v-card-actions>
+        <v-card color="#26314C" height="100">
+          <v-layout row justify-space-between class="ma-0">
+           <v-flex md12 class="vl" >
+              
+              <div class="pa-6">
+                <div class="headline font-weight-thin white--text">Expenses</div>
+                <div class="font-weight-thin white--text"> Ksh 1234</div>
+              </div>
+            </v-flex>
+            <v-flex xs3>
+              <v-icon class="ma-4" color="#ff8133" size="65">> mdi-cash-minus</v-icon>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
       <v-flex class="pa-2" md4 xs12>
-        <v-card color="#26314C" height="200">
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="#ff8133">
-                <v-icon class="mr-1 white--text">mdi-email-open </v-icon>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-bold white--text">
-                  Net
-                </v-list-item-title>
-                <v-list-item-subtitle class="white--text"
-                  >6</v-list-item-subtitle
-                >
-              </v-list-item-content>
-
-              <v-row align="center" justify="end"> </v-row>
-            </v-list-item>
-          </v-card-actions>
+        <v-card color="#26314C" height="100">
+          <v-layout row justify-space-between class="ma-0">
+            <v-flex md12 class="vl" >
+              <div class="pa-6">
+                <div class="headline font-weight-thin white--text">Net</div>
+                <div class="font-weight-thin white--text"> 4</div>
+              </div>
+            </v-flex>
+            
+          </v-layout>
         </v-card>
       </v-flex>
     </v-layout>
     <v-layout row class="mt-2">
       <v-flex class="pa-2" md6 sm12>
         <v-card class="card-one">
-          <v-card-text class="card-one white--text">
-            <h2 class="card-title font-weight-bold mt-2 ml-2">Mpesa Shares</h2>
-
-            <p class="d-inline-flex font-weight-bold ml-2 mt-1">87,850</p>
-          </v-card-text>
+          <v-layout row justify-space-between class="ma-0">
+            <v-flex xs9 >
+              <div class="pa-6">
+                <div class="headline font-weight-thin white--text">Mpesa Shares</div>
+                <div class="font-weight-thin white--text"> 441,234</div>
+              </div>
+            </v-flex>
+            <v-flex xs3>
+              <v-icon class="ma-4" color="#ff8133" size="65">> mdi-cash-plus</v-icon>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
       <v-flex class="pa-2" md6 sm12>
         <v-card class="card-one">
-          <v-list-item class="grow">
-            <v-list-item-avatar color="#ff8133">
-              <v-icon class="mr-1 white--text">mdi-notebook-edit</v-icon>
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-bold white--text">
-                Referrers
-              </v-list-item-title>
-              <v-list-item-subtitle class="white--text">0</v-list-item-subtitle>
-            </v-list-item-content>
-
-            <v-row align="center" justify="end"> </v-row>
-          </v-list-item>
+         <v-layout row justify-space-between class="ma-0">
+            <v-flex xs9 >
+              <div class="pa-6">
+                <div class="headline font-weight-thin white--text">Referers</div>
+                <div class="font-weight-thin white--text">34</div>
+              </div>
+            </v-flex>
+            <v-flex xs3>
+              <v-icon class="ma-4" color="#ff8133" size="65">> mdi-notebook-edit</v-icon>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
     </v-layout>
 
     <v-layout row>
-      <v-flex md6 xs 12 class="pa-2">
-        
-          <v-card elevation="24" class="card-one mb-4">
-            <v-toolbar dark color="#202941">
-              <v-toolbar-title>Outgoing</v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-chip class="ma-2" color="primary">
-                Outgoing share by status
-              </v-chip>
-            </v-toolbar>
-
-            <v-row class="white--text" align="center">
-              <v-flex >
-                <v-progress-circular
-                  :rotate="-90"
-                  :size="100"
-                  :width="15"
-                  :value="value"
-                  color="primary"
-                  class="ml-12 mt-6"
-                >
-                  {{ value }}
-                </v-progress-circular></v-flex
-              >
-              <v-flex>
-                <v-row class="mt-4"
-                  ><div class="hr mt-2 success"></div>
-                  <span class="ml-4"> 0% Confirmed</span>
-                </v-row>
-
-                <v-row
-                  ><div class="hr mt-2 warning"></div>
-                  <span class="ml-4"> 0% Paired</span>
-                </v-row>
-                <v-row
-                  ><div class="hr mt-2 info"></div>
-                  <span class="ml-4"> 99% Pending</span>
-                </v-row></v-flex
-              >
-            </v-row>
-          </v-card>
-    
-      
-          <v-card elevation="24" class="card-one">
-            <v-toolbar dark color="#202941">
-              <v-toolbar-title>Incoming</v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-chip class="ma-2" color="primary">
-                Incoming share by status
-              </v-chip>
-            </v-toolbar>
-            <v-row class="white--text" align="center">
-              <v-flex>
-                <v-progress-circular
-                  :rotate="-90"
-                  :size="100"
-                  :width="15"
-                  :value="value"
-                  color="primary"
-                  class="ml-12 mt-6"
-                >
-                  {{ value }}
-                </v-progress-circular></v-flex
-              >
-              <v-col cols="6">
-                <v-row class="mt-4"
-                  ><div class="hr mt-2 success"></div>
-                  <span class="ml-4"> 0% Confirmed</span>
-                </v-row>
-
-                <v-row
-                  ><div class="hr mt-2 warning"></div>
-                  <span class="ml-4"> 0% Paired</span>
-                </v-row>
-                <v-row
-                  ><div class="hr mt-2 info"></div>
-                  <span class="ml-4"> 99% Pending</span>
-                </v-row></v-col
-              >
-            </v-row>
-          </v-card>
-      
-      </v-flex>
-
       <v-flex class="pa-2" md6 sm12>
         <v-card elevation="24" color="#26314C" height="100%">
           <v-toolbar dark color="#202941" class="mb-4">
             <v-toolbar-title>Mpesa Auction</v-toolbar-title>
           </v-toolbar>
           <v-row>
-           >
-              <v-card-text class="white--text">
-                <div class="text-center">
-                  <no-ssr>
-                    <flip-countdown
-                      deadline="2021-02-25 00:00:00 "
-                      :showDays="false"
-                    ></flip-countdown>
-                  </no-ssr>
-                  <h2 class="card-title font-weight-bold mt-4">
-                    Left To Open Bid
-                  </h2>
-                </div>
-              </v-card-text>
-            
+            >
+            <v-card-text class="white--text">
+              <div class="text-center">
+                <no-ssr>
+                  <flip-countdown
+                    deadline="2021-02-25 00:00:00 "
+                    :showDays="false"
+                  ></flip-countdown>
+                </no-ssr>
+                <h2 class="card-title font-weight-bold mt-4">
+                  Left To Open Bid
+                </h2>
+              </div>
+            </v-card-text>
+          </v-row>
+        </v-card>
+      </v-flex>
+      <v-flex md6 xs 12 class="pa-2">
+        <v-card elevation="24" class="card-one mb-4">
+          <v-toolbar dark color="#202941">
+            <v-toolbar-title>Outgoing</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-chip class="ma-2" color="primary">
+              Outgoing share by status
+            </v-chip>
+          </v-toolbar>
+
+          <v-row class="white--text" align="center">
+            <v-flex>
+              <v-progress-circular
+                :rotate="-90"
+                :size="100"
+                :width="15"
+                :value="value"
+                color="primary"
+                class="ml-12 mt-6"
+              >
+                {{ value }}
+              </v-progress-circular></v-flex
+            >
+            <v-flex>
+              <v-row class="mt-4"
+                ><div class="hr mt-2 success"></div>
+                <span class="ml-4"> 0% Confirmed</span>
+              </v-row>
+
+              <v-row
+                ><div class="hr mt-2 warning"></div>
+                <span class="ml-4"> 0% Paired</span>
+              </v-row>
+              <v-row
+                ><div class="hr mt-2 info"></div>
+                <span class="ml-4"> 99% Pending</span>
+              </v-row></v-flex
+            >
+          </v-row>
+        </v-card>
+
+        <v-card elevation="24" class="card-one">
+          <v-toolbar dark color="#202941">
+            <v-toolbar-title>Incoming</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-chip class="ma-2" color="primary">
+              Incoming share by status
+            </v-chip>
+          </v-toolbar>
+          <v-row class="white--text" align="center">
+            <v-flex>
+              <v-progress-circular
+                :rotate="-90"
+                :size="100"
+                :width="15"
+                :value="value"
+                color="primary"
+                class="ml-12 mt-6"
+              >
+                {{ value }}
+              </v-progress-circular></v-flex
+            >
+            <v-col cols="6">
+              <v-row class="mt-4"
+                ><div class="hr mt-2 success"></div>
+                <span class="ml-4"> 0% Confirmed</span>
+              </v-row>
+
+              <v-row
+                ><div class="hr mt-2 warning"></div>
+                <span class="ml-4"> 0% Paired</span>
+              </v-row>
+              <v-row
+                ><div class="hr mt-2 info"></div>
+                <span class="ml-4"> 99% Pending</span>
+              </v-row></v-col
+            >
           </v-row>
         </v-card>
       </v-flex>
@@ -243,7 +220,7 @@ export default {
       interval: {},
       labels: ["12am", "3am", "6am", "9am", "12pm", "3pm", "6pm", "9pm"],
       value: 99,
-      value1: [0, 2, 5, 9, 5, 10, 3, 5, -4, -10, 1, 8, 2, 9, 0],
+      sparklineData: [423, 446, 675, 510, 590, 610, 423],
     };
   },
 };
@@ -258,8 +235,8 @@ export default {
   background-color: #26314c;
 }
 .vl {
-  border-left: 6px solid #161d31;
-  height: 50px;
+  border-left: 6px solid cyan;
+  height: 100px;
 }
 .hr {
   border-bottom: 8px solid;
@@ -289,5 +266,8 @@ export default {
 .stats {
   width: 100%;
   height: 200px;
+}
+.txt{
+  font-family: 'PT Sans', sans-serif;
 }
 </style>
